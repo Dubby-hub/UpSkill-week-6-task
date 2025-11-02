6db9515b1568485334388233af4071ba913acd56
chukwudubem <ndukwechukwudubem@gmail.com>
2025-11-02 22:26:55 +0100

    last commit

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
-const interns = [
-  {
-    name: "Chika Okafor",
-    Scores: [85, 92, 78, 88, 90],
-  },
-  {
-    name: "Esther Odums",
-    Scores: [40, 98, 27, 89, 45],
-  },
-  {
-    name: "Toby Aki",
-    Scores: [12, 9, 37, 16, 51],
-  },
-  {
-    name: "Bruce Okoye",
-    Scores: [32, 45, 28, 39, 41],
-  },
-  {
-    name: "Iniobong Edidiong",
-    Scores: [75, 82, 68, 80, 77],
-  },
+// Array of Students as objects
+let students = [
+  { name: "Ekene Okafor", 
+    scores: { math: 90, english: 85, biology: 88, chemistry: 78, geography: 85 } 
+},
+
+ { name: "John Ebube", 
+    scores: { math: 90, english: 85, biology: 88, chemistry: 78, geography: 85 } 
+},
+
+ { name: "Esther Ike", 
+    scores: { math: 90, english: 85, biology: 88, chemistry: 78, geography: 85 } 
+},
+
+ { name: "Toby Kings", 
+    scores: { math: 90, english: 85, biology: 88, chemistry: 78, geography: 85 } 
+},
+
+ { name: "Emmanuel Johnson", 
+    scores: { math: 90, english: 85, biology: 88, chemistry: 78, geography: 85 } 
+},
+ { name: "Precious Duru", 
+    scores: { math: 90, english: 85, biology: 88, chemistry: 78, geography: 85 } 
+},
+   
+
 ];
 
 // Function to calculate average score
-function calculateAverage(Scores) {
-  let sum = 0;
-  for (let i = 0; i < Scores.length; i++) {
-    sum = sum + Scores[i];
-  }
-
-  let average = sum / Scores.length;
-  return average;
+function calculateAverage(student) {
+  let scores = Object.values(student.scores);
+  let total = scores.reduce((sum, score) => sum + score, 0);
+  return total / scores.length;
 }
 
-interns.forEach(function (intern) {
-  let averageScore = calculateAverage(intern.Scores);
-  console.log(intern.name, averageScore);
+// Function to dispay result
+students.forEach(student => {
+  console.log(student.name + "'s average is " + calculateAverage(student));
 });
 
- function showMessage(){
+
+// Function to make button clickable for directions
+function showMessage(){
     alert("HI this is a console gradebook. please open your console.");
     alert("Quick Tip:For windows press shift + ctrl + J")
   }
